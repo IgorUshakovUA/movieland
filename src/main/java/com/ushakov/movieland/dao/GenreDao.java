b49e3f66f613dd10773ee164bdab368366d10e64
Igor Ushakov <igor-ushakov@ukr.net>
2018-11-18 16:04:01 +0200

    Initial commit

---
+++ src/main/java/com/ushakov/movieland/dao/GenreDao.java
@@ -0,0 +1,9 @@
+package com.ushakov.movieland.dao;
+
+import com.ushakov.movieland.entity.Genre;
+
+import java.util.List;
+
+public interface GenreDao {
+    List<Genre> getAll();
+}
--- src/main/java/com/ushakov/movieland/service/DefaultMovieService.java
+++ src/main/java/com/ushakov/movieland/service/DefaultMovieService.java
@@ -2,6 +2,7 @@ package com.ushakov.movieland.service;
 
 import com.ushakov.movieland.dao.MovieDao;
 import com.ushakov.movieland.entity.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public class DefaultMovieService implements MovieService {
     private MovieDao movieDao;
 
+    @Autowired
     public DefaultMovieService(MovieDao movieDao) {
         this.movieDao = movieDao;
     }
