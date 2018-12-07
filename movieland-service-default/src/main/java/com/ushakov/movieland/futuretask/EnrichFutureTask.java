b855fb49491a9cb2334f408d172b3036e30c2eed
Igor Ushakov <igor-ushakov@ukr.net>
2018-12-07 11:57:19 +0200

    [x-1] changes after sonarcloud checks

--- movieland-service-default/src/main/java/com/ushakov/movieland/futuretask/EnrichFutureTask.java
+++ movieland-service-default/src/main/java/com/ushakov/movieland/futuretask/EnrichFutureTask.java
@@ -3,11 +3,8 @@ package com.ushakov.movieland.futuretask;
 import java.util.concurrent.FutureTask;
 
 public class EnrichFutureTask<V> extends FutureTask<V> {
-    private int movieId;
 
     public EnrichFutureTask(int movieId, EnrichCall<V> enrichCall) {
         super(() -> enrichCall.call(movieId));
-
-        this.movieId = movieId;
     }
 }
